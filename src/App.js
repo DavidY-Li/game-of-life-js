4f95f0259e3fabb709545ebd0845b365a079379b
David Li <davidliepic@gmail.com>
2024-01-03 12:03:23 -0500

    Functional game

--- src/App.js
+++ src/App.js
@@ -11,81 +11,15 @@ function Cell({ alive, onCellPress, onCellHover }) {
   );
 }
 
-function Board() {
-  const len = 25;
-  const height = 25;
-
-  const [currentBoard, setBoard] = useState(Array(len * len).fill(false));
-  const [mouseDown, setMouseDown] = useState(false);
-  const [isOff, setOff] = useState(null);
-
-  function handleClick(x, y) {
-    setOff(!currentBoard[x + y * len]);
-    checkAlive(x + y * len);
-
-    // change current tile immediately
-    const newBoard = currentBoard.slice();
-
-    newBoard[x + y * len] = !currentBoard[x + y * len];
-    setBoard(newBoard);
-  }
-
-  function updateBoard() {
-    const newBoard = currentBoard.slice();
-
-    for (let i = 0; i < currentBoard.length; i++) {
-      newBoard[i] = checkAlive(i);
-    }
-    setBoard(newBoard);
-  }
-
-  function handleHover(x, y) {
-    // check if mouse is pressed and tile isn't already in correct state
-    if (!mouseDown || currentBoard[x + y * len] == isOff) return;
-
-    // change current tile
-    const newBoard = currentBoard.slice();
-
-    newBoard[x + y * len] = isOff;
-    setBoard(newBoard);
-  }
-
-  // see if cells are alive
-  function checkAlive(position) {
-    // get x and y from absolute position
-    let x = position % len;
-    let y = Math.floor(position / len);
-    let around = 0;
-
-    // iterate through all around
-    for (let i = -1; i < 2; i++) {
-      for (let j = -1; j < 2; j++) {
-        if (i == 0 && j == 0) {
-          continue;
-        }
-
-        // get current squarecurrentSquare
-        // modulo in js is weird for negatives :/
-        let currentSquare =
-          ((((x + i) % len) + len) % len) +
-          ((((y + j) % height) + height) % height) * height;
-
-        // update count
-        if (currentBoard[currentSquare] == true) {
-          around++;
-        }
-      }
-    }
-
-    if (currentBoard[position] == true && (around == 2 || around == 3)) {
-      return true;
-    } else if (currentBoard[position] == false && around == 3) {
-      return true;
-    } else {
-      return false; // die
-    }
-  }
-
+function Board({
+  currentBoard,
+  len,
+  height,
+  handleClick,
+  handleHover,
+  setMouseDown,
+  setOff,
+}) {
   // size
   const rows = [...Array(len).keys()];
   const cols = [...Array(height).keys()];
@@ -120,19 +54,120 @@ function Board() {
 }
 
 export default function Game() {
+  const len = 25;
+  const height = 25;
+
+  const [currentBoard, setBoard] = useState(Array(len * height).fill(false));
   const [playing, setPlaying] = useState(false);
+  const [mouseDown, setMouseDown] = useState(false);
+  const [isOff, setOff] = useState(null);
+  const [intervalState, setIntervalState] = useState(null);
+
+  function handleClick(x, y) {
+    setOff(!currentBoard[x + y * len]);
+    updateCell(x + y * len, currentBoard, len, height);
+
+    // change current tile immediately
+    const newBoard = currentBoard.slice();
+
+    newBoard[x + y * len] = !currentBoard[x + y * len];
+    setBoard(newBoard);
+  }
+
+  function handleHover(x, y) {
+    // check if mouse is pressed and tile isn't already in correct state
+    if (!mouseDown || currentBoard[x + y * len] == isOff) return;
+
+    // change current tile
+    const newBoard = currentBoard.slice();
+
+    newBoard[x + y * len] = isOff;
+    setBoard(newBoard);
+  }
+
+  // update entire board
+  function updateBoard() {
+    setBoard((oldBoard) => {
+      const newBoard = oldBoard.slice();
+
+      for (let i = 0; i < oldBoard.length; i++) {
+        newBoard[i] = updateCell(i, oldBoard, len, height);
+      }
+      return newBoard;
+    });
+  }
+
+  function clearBoard() {
+    const newBoard = currentBoard.slice();
+
+    for (let i = 0; i < currentBoard.length; i++) {
+      newBoard[i] = false;
+    }
+    setBoard(newBoard);
+  }
 
   function toggleGame() {
+    if (playing) {
+      clearInterval(intervalState);
+    } else {
+      const interval = setInterval(updateBoard, 50);
+      setIntervalState(interval);
+    }
     setPlaying((play) => !play);
   }
 
   return (
     <>
-      <button onClick={toggleGame}>{playing ? "Play" : "Pause"}</button>
-
+      <button onClick={updateBoard}>{"Next generation"}</button>
+      <button onClick={toggleGame}>{playing ? "Pause" : "Play"}</button>
+      <button onClick={clearBoard}>{"Clear"}</button>
       <div>
-        <Board />
+        <Board
+          currentBoard={currentBoard}
+          len={len}
+          height={height}
+          handleClick={handleClick}
+          handleHover={handleHover}
+          setMouseDown={setMouseDown}
+          setOff={setOff}
+        />
       </div>
     </>
   );
+}
+
+// see if cells are alive
+function updateCell(position, currentBoard, len, height) {
+  // get x and y from absolute position
+  let x = position % len;
+  let y = Math.floor(position / len);
+  let around = 0;
+
+  // iterate through all around
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      if (i == 0 && j == 0) {
+        continue;
+      }
+
+      // get current squarecurrentSquare
+      // modulo in js is weird for negatives :/
+      let currentSquare =
+        ((((x + i) % len) + len) % len) +
+        ((((y + j) % height) + height) % height) * height;
+
+      // update count
+      if (currentBoard[currentSquare]) {
+        around++;
+      }
+    }
+  }
+
+  if (currentBoard[position] == true && (around == 2 || around == 3)) {
+    return true;
+  } else if (currentBoard[position] == false && around == 3) {
+    return true;
+  } else {
+    return false; // die
+  }
 }
